dec5711be69bd9692bcf10cdc8b9d7b61c8d58cc
Thomas Burke <piecraft24@gmail.com>
2022-06-03 13:12:00 +0100

    Updated: Method of checking messages

--- src/main/java/kineticnetwork/net/chat/Utils/TextCheck.java
+++ src/main/java/kineticnetwork/net/chat/Utils/TextCheck.java
@@ -1,5 +1,6 @@
 package kineticnetwork.net.chat.Utils;
 
+import com.sun.istack.internal.logging.Logger;
 import kineticnetwork.net.chat.Chat;
 import kineticnetwork.net.chat.Files.FileEditor;
 import kineticnetwork.net.chat.Files.FilePrep;
@@ -18,7 +19,7 @@ public class TextCheck {
     Notify notify = new Notify();
     FilePrep fp = new FilePrep();
 
-    public boolean checkmessage(String input, Player player, String source, String extra) throws IOException {
+    /* public boolean checkmessage(String input, Player player, String source, String extra) throws IOException {
         input = input.toLowerCase();
         String[] sepword = input.split("\\s+");
 
@@ -56,7 +57,7 @@ public class TextCheck {
         return true;
     }
 
-    public boolean possibleflag(Player player, String key, String value, String source, String extra, String input) throws IOException { //Used for chat if word is contained with in another word
+   public boolean possibleflag(Player player, String key, String value, String source, String extra, String input) throws IOException { //Used for chat if word is contained with in another word
         reports.report(player.getName(), key, input, source, value);// adds report
         if (fp.isPlayerFlaged(player) == false) {
             reports.flagPlayer(player);
@@ -64,5 +65,77 @@ public class TextCheck {
         notify.imformStaffPossibleflag(player, input, key, source, value);
         Chat.getLogger().info(Chat.Prefix + ": " + player.getName() + " Has been flagged :" + key + " \n Message " + input + " Reason: " + value);
         return true;
+    }*/
+    //New
+    private String extra = "null";//Any extra things such as sign cords
+
+
+    public int checkmessage(String input, Player player, String source, String extra) throws IOException {
+       // Logger.getLogger("Debug").info(player.getName());
+        String[] sepword = removeInvalids(input).split("\\s+");
+
+        for (int i = 0; i < sepword.length; i++) {
+
+            Iterator hmIterator = Chat.Blacklisted.entrySet().iterator();
+            while (hmIterator.hasNext()) {
+                Map.Entry mapElement = (Map.Entry) hmIterator.next();
+                if (sepword[i].contains((CharSequence) mapElement.getKey())) {
+                    if (source != "Chat") {
+                        mutePlayer(player, mapElement.getKey().toString(), mapElement.getValue().toString(), source, extra, input);
+                        return 2;
+                    }
+                    if (sepword[i].equalsIgnoreCase(String.valueOf(mapElement.getKey()))) { // word is by it self blocked use method1
+                        // notfiy.sendresponse(player, mapElement.getValue().toString());// sends mute message
+                        mutePlayer(player, mapElement.getKey().toString(), mapElement.getValue().toString(), source, extra, input);
+                        return 2;
+                    } else {
+                        possibleflag(player, mapElement.getKey().toString(), mapElement.getValue().toString(), source, extra, input);
+                        return 1;
+                    }
+                }
+
+            }
+
+        }
+        return 0;
+    }
+
+    public boolean mutePlayer(Player player, String key, String value, String source, String extra, String input) throws IOException {
+        reports.report(player.getName(), key, input, source, value);// adds report
+        reports.mutePlayer(player);
+            notify.imformPlayerIsMuted(player, value);
+        if (!value.contains("Blacklisted Term")) {
+
+           // notfiy.staff(player, input, key, value, source, extra, true);
+            notify.staff(player, input, key,value, source,extra, true);
+        }else {
+         //   notify.imformStaff(player, "Message removed for security", "BlackListed Term", source, value);
+            notify.staff(player, "Message removed for security", "BlackListed Term","Higher Action", source,extra, true);
+        }
+        return true;
+    }
+
+    public boolean possibleflag(Player player, String key, String value, String source, String extra, String input) throws IOException { //Used for chat if word is contained with in another word
+        reports.report(player.getName(), key, input, source, value);// adds report
+        reports.flagPlayer(player);// adds tp watch list
+        if (!value.contains("Blacklisted Term")) {
+          //  notfiy.staff(player, input, key, value, source, extra, false);
+            notify.staff(player, input, key,value, source,extra, false);
+        }else {
+           // notfiy.staff(player, "Message removed for security", "BlackListed Term", "Higher Action", source, extra, false);
+           // notify.staff(player, "Message removed for security", "BlackListed Term", source, value);
+            notify.staff(player, "Message removed for security", "BlackListed Term","Higher Action", source,extra, false);
+        }
+//Todo Maybe change how the messeage is so can use spigot code, Eg notify.staff() instead of possible and imform staff.
+        return true;
+    }
+
+    private String removeInvalids(String input) {
+        String mString;
+        mString = input.replace("?", "");
+        mString = mString.replace(".", " "); //To avoid merging messages together its a space
+        mString = mString.replace(",", " ");
+
+        return mString;
     }
 }
